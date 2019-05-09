cced719dd4fd00bd3374c4c4a59831becea10566
joaquinto <roejoeodj12@gmail.com>
2019-05-10 00:58:00 +0100

    feature(setup swagger): add swagger.js file    [Finishes  #165926543]

---
+++ configuration/swagger.js
@@ -0,0 +1,28 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const swaggerDefinition = {
+  info: {
+    title: 'Quick Credit',
+    version: '1.0.0',
+    description: 'Quick Credit is an online lending platform that provides short term soft loans to individuals',
+  },
+  securityDefinitions: {
+    bearerAuth: {
+      type: 'apiKey',
+      name: 'Authorization',
+      scheme: 'bearer',
+      in: 'header',
+    },
+  },
+  host: 'quickycredit.herokuapp.com',
+  basePath: '/api/v1',
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./swagger/*.yaml'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export default swaggerSpec;
