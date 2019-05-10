25d7b87c6f8f5b33c9c058f1b3360c2fad04362c
joaquinto <roejoeodj12@gmail.com>
2019-05-10 15:11:42 +0100

    bug(fix Error): Fix error in user controller file   [Finishes  #165937679]

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -42,7 +42,7 @@ export default {
   sendResetPasswordLink: (req, res) => {
     userModule.sendResetPassword(req)
       .then(() => {
-        res.status(200).json({ status: 200, data: 'check your email for a password reset link' });
+        res.status(200).json({ status: 200, data: { message: 'check your email for a password reset link', token: data.token, email: data.email } });
       }).catch(error => res.status(500).json({ status: 500, error: error.message }));
   },
 
