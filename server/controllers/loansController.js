1036c9523a9aa5dc380c01285ed5648d7952a5f7
joaquinto <roejoeodj12@gmail.com>
2019-05-19 21:05:47 +0100

    feature(loan application): added loan application endpoint with postgres  [Finish #166108308]

--- server/controllers/loansController.js
+++ server/controllers/loansController.js
@@ -1,11 +1,47 @@
 import loanModule from '../module/loansModule';
+import utilities from '../helpers/utilities';
+import loans from '../model/loans';
+import db from '../db/index';
+
+const { query } = db;
+const { createLoan } = loans;
 
 export default class LoanController {
-  static createLoan(req, res) {
-    loanModule.createLoan(req)
-      .then((data) => {
-        res.status(201).json({ status: 201, data, message: 'Loan created successfully' });
-      });
+  static async createLoan(req, res, next) {
+    const { firstname, lastname } = req.body;
+    const email = req.body.email.toLowerCase();
+    const createdOn = new Date();
+    const status = 'pending';
+    let { tenor, amount } = req.body;
+    tenor = Number(tenor);
+    amount = Number(amount);
+    const interest = utilities.interest(amount);
+    const paymentInstallment = utilities.paymentInstallment(amount, interest, tenor);
+    amount += interest;
+    const balance = amount;
+    const values = [req.decoded.id, firstname, lastname, email,
+      createdOn, status, tenor, amount, interest, paymentInstallment,
+      balance];
+    try {
+      const { rows } = await query(createLoan, values);
+      const response = {
+        id: rows[0].id,
+        firstname: rows[0].first_name,
+        lastname: rows[0].last_name,
+        email: rows[0].email,
+        createdOn: rows[0].created_on,
+        status: rows[0].status,
+        repaid: rows[0].repaid,
+        tenor: rows[0].tenor,
+        amount: rows[0].amount,
+        paymentInstallment: rows[0].payment_installment,
+        balance: rows[0].balance,
+        interest: rows[0].interest,
+      };
+      res.status(201).json({ status: 201, message: 'Loan created successfully', data: response });
+    } catch (error) {
+      next(error);
+    }
   }
 
   static getAllLoans(req, res) {
--- server/model/loans.js
+++ server/model/loans.js
@@ -1,48 +1,58 @@
-import objectUtils from '../helpers/objectUtils';
+const loanQueries = {
+  createLoan: `INSERT INTO 
+  loans(user_id, first_name, last_name, email, 
+    created_on, status, tenor, amount, interest, 
+    payment_installment, balance) 
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) 
+    RETURNING *`,
+  getLoanByUserId: 'SELECT * FROM loans WHERE user_id = $1',
+};
 
-export default class Loans {
-  constructor(id, firstname, lastname, email, createdOn, status, repaid,
-    tenor, amount, paymentInstallment, balance, interest) {
-    this.id = id;
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.email = email;
-    this.createdOn = createdOn;
-    this.status = status;
-    this.repaid = repaid;
-    this.tenor = tenor;
-    this.amount = amount;
-    this.paymentInstallment = paymentInstallment;
-    this.balance = balance;
-    this.interest = interest;
-  }
+export default loanQueries;
 
-  static getLoans(loans) {
-    return loans;
-  }
+// export default class Loans {
+//   constructor(id, firstname, lastname, email, createdOn, status, repaid,
+//     tenor, amount, paymentInstallment, balance, interest) {
+//     this.id = id;
+//     this.firstname = firstname;
+//     this.lastname = lastname;
+//     this.email = email;
+//     this.createdOn = createdOn;
+//     this.status = status;
+//     this.repaid = repaid;
+//     this.tenor = tenor;
+//     this.amount = amount;
+//     this.paymentInstallment = paymentInstallment;
+//     this.balance = balance;
+//     this.interest = interest;
+//   }
 
-  static getConditionalLoans(loans, status, repaid) {
-    const newLoan = loans
-      .filter(loan => ((loan.status === status) && (loan.repaid === JSON.parse(repaid))));
-    return newLoan;
-  }
+//   static getLoans(loans) {
+//     return loans;
+//   }
 
-  static getLoanByEmail(loans, email) {
-    const newLoan = loans.filter(loan => (loan.email === email));
-    return newLoan;
-  }
+//   static getConditionalLoans(loans, status, repaid) {
+//     const newLoan = loans
+//       .filter(loan => ((loan.status === status) && (loan.repaid === JSON.parse(repaid))));
+//     return newLoan;
+//   }
 
-  static getLoanById(loans, id) {
-    return loans.filter(loan => (loan.id === id));
-  }
+//   static getLoanByEmail(loans, email) {
+//     const newLoan = loans.filter(loan => (loan.email === email));
+//     return newLoan;
+//   }
 
-  static editLoanStatusById(loans, id, status) {
-    const newLoan = objectUtils.loanManipulation(loans, id, status);
-    return newLoan;
-  }
+//   static getLoanById(loans, id) {
+//     return loans.filter(loan => (loan.id === id));
+//   }
 
-  static getNewBalance(loans, id, balance) {
-    const newLoan = objectUtils.loanManipulation(loans, id, balance);
-    return newLoan;
-  }
-}
+//   static editLoanStatusById(loans, id, status) {
+//     const newLoan = objectUtils.loanManipulation(loans, id, status);
+//     return newLoan;
+//   }
+
+//   static getNewBalance(loans, id, balance) {
+//     const newLoan = objectUtils.loanManipulation(loans, id, balance);
+//     return newLoan;
+//   }
+// }
