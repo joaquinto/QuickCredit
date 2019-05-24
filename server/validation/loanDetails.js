434f7332e467390df975a5673776fd813c749401
joaquinto <roejoeodj12@gmail.com>
2019-05-24 07:16:53 +0100

    bug(fix issues): add some variables to the loan controller   [Finishes #166231455]

--- server/validation/loanDetails.js
+++ server/validation/loanDetails.js
@@ -1,9 +1,6 @@
 import Joi from '@hapi/joi';
 
 const loanDetails = Joi.object().keys({
-  firstname: Joi.string().min(2).required(),
-  lastname: Joi.string().min(2).required(),
-  email: Joi.string().email({ mindomainSegments: 2 }).required(),
   tenor: Joi.number().min(1).max(12).required(),
   amount: Joi.number().min(500).required(),
 });
